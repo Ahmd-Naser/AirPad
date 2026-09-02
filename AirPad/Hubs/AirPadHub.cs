4f645ad1a5546fe33c95696de37d90440e1a59d6
ahmd-naser <ahmdnasr197@gmail.com>
2026-09-02 06:57:31 +0300

    added hub

---
+++ AirPad/Hubs/AirPadHub.cs
@@ -0,0 +1,27 @@
+﻿using AirPad.Models;
+using AirPad.Services;
+using Microsoft.AspNetCore.SignalR;
+
+namespace AirPad.Hubs;
+
+public class AirPadHub : Hub
+{
+    private readonly IMouseSimulator _mouseSimulator;
+
+    public AirPadHub(IMouseSimulator mouseSimulator)
+    {
+        _mouseSimulator = mouseSimulator;
+    }
+
+    public async Task SendMovement(TouchPayload payload)
+    {
+        _mouseSimulator.ProcessMovement(payload);
+        await Task.CompletedTask; 
+    }
+
+    public async Task SendCommand(MacroCommand command)
+    {
+        _mouseSimulator.ProcessCommand(command);
+        await Task.CompletedTask;
+    }
+}
